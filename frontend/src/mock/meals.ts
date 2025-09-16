85d161d79d90b19c37587564ddab27502e7bcf93
Ken <kzou55@gmail.com>
2025-09-16 19:15:03 -0400

    added mock data for meals just to build protype of meal page

--- frontend/src/mock/meals.ts
+++ frontend/src/mock/meals.ts
@@ -1,29 +1,38 @@
-import type { Meal } from "../types/meal"
+import type { Meal } from "../types/meal";
 
-export const mockMeal: Meal[] = [{
+export const mockMeal: Meal[] = [
+  {
     id: 1,
-    type: "Lunch",
-    date: "2025-09-11",
+    type: "Breakfast",
+    date: "2025-09-16",
     mealFoodEntries: [
-        {
-            id: 1,
-            quantity: 2,
-            foodItem: {
-                id: 1,
-                name: "chicken",
-                calories: 5,
-                protein: 1
-            }
-        },
-        {
-            id: 2,
-            quantity: 2,
-            foodItem: {
-                id: 1,
-                name: "chicken",
-                calories: 5,
-                protein: 1
-            }
-        }
-    ]
-}]
+      {
+        id: 101,
+        foodItem: { id: 1, name: "Oatmeal", calories: 150, protein: 5},
+        quantity: 1,
+      },
+      {
+        id: 102,
+        foodItem: { id: 2, name: "Banana", calories: 105, protein: 1},
+        quantity: 1,
+      },
+    ],
+  },
+  {
+    id: 2, 
+    type: "Lunch",
+    date: "2025-09-16",
+    mealFoodEntries: [
+      {
+        id: 103,
+        foodItem: { id: 3, name: "Spaghetti", calories: 600, protein: 53},
+        quantity: 1,
+      },
+      {
+        id: 104,
+        foodItem: { id: 4, name: "Smoothie", calories: 105, protein: 1},
+        quantity: 1,
+      },
+    ],
+  },
+];
--- frontend/src/pages/AddMealPage.tsx
+++ frontend/src/pages/AddMealPage.tsx
@@ -1,85 +1,46 @@
 import { Link } from "react-router-dom";
+import { mockMeal } from "../mock/meals";
 
 const AddMealPage = () => {
+
     return (
         <main className="flex flex-col">
             Add a Meal
             <table className="min-w-full border-collapse border border-gray-300">
                 <thead>
                     <tr className="bg-blue-700 text-white">
-                        <th className="border border-gray-300 px-4 py-2">Breakfast</th>
+                        <th className="border border-gray-300 px-4 py-2"></th>
                         <th className="border border-gray-300 px-4 py-2">Calories kcal</th>
                         <th className="border border-gray-300 px-4 py-2">Protein g</th>
                         <th className="border border-gray-300 px-4 py-2"></th> {/* for delete icon */}
                     </tr>
                 </thead>
-                <tbody>
-                    <tr className="border border-gray-300">
-                        <th className="border border-gray-300 px-4 py-2">Food Name</th>
-                        <td className="border border-gray-300 px-4 py-2">209</td>
-                        <td className="border border-gray-300 px-4 py-2">41</td>
-                        <td className="border border-gray-300 px-4 py-2 text-center">
-                            <button className="text-red-600">🗑️</button>
-                        </td>
-                    </tr>
-                    <tr className="border border-gray-300">
-                        
-                        <th className="border border-gray-300 px-4 py-2">Food Name</th>
-                        <td className="border border-gray-300 px-4 py-2">209</td>
-                        <td className="border border-gray-300 px-4 py-2">41</td>
-                        <td className="border border-gray-300 px-4 py-2 text-center">
-                            <button className="text-red-600">🗑️</button>
-                        </td>
-                    </tr>
+                <tbody >
+                    {mockMeal.map((meal) => (
+                        <>
+                            <tr className="border border-gray-300">
+                                <td className="border border-gray-300">
+                                    {meal.type}
+                                </td>
+                            </tr>
+                            {meal.mealFoodEntries.map((entry) => (
+                                <tr>
+                                    <td className="border border-gray-300">{entry.foodItem.name}</td>
+                                    <td className="border border-gray-300">{entry.foodItem.calories}</td>
+                                    <td className="border border-gray-300">{entry.foodItem.protein}</td>
+                                    <td className="border border-gray-300"><button>Delete</button></td>
+                                </tr>
+
+                            ))}
+
+                        </>
+                    ))}
                     <tr>
                         <td>
                             <Link to="/add-food" style={{ marginRight: "1rem" }}>Add Food</Link></td>
                     </tr>
-                    <tr>
-                        <th>Lunch</th>
-                    </tr>
-                    <tr className="border border-gray-300">
-                        
-                        <th className="border border-gray-300 px-4 py-2">Food Name</th>
-                        <td className="border border-gray-300 px-4 py-2">209</td>
-                        <td className="border border-gray-300 px-4 py-2">41</td>
-                        <td className="border border-gray-300 px-4 py-2 text-center">
-                            <button className="text-red-600">🗑️</button>
-                        </td>
-                    </tr>
-                    <tr className="border border-gray-300">
-                        
-                        <th className="border border-gray-300 px-4 py-2">Food Name</th>
-                        <td className="border border-gray-300 px-4 py-2">209</td>
-                        <td className="border border-gray-300 px-4 py-2">41</td>
-                        <td className="border border-gray-300 px-4 py-2 text-center">
-                            <button className="text-red-600">🗑️</button>
-                        </td>
-                    </tr><tr>
-                        <th>Dinner</th>
-                    </tr>
-                    <tr className="border border-gray-300">
-                        
-                        <th className="border border-gray-300 px-4 py-2">Food Name</th>
-                        <td className="border border-gray-300 px-4 py-2">209</td>
-                        <td className="border border-gray-300 px-4 py-2">41</td>
-                        <td className="border border-gray-300 px-4 py-2 text-center">
-                            <button className="text-red-600">🗑️</button>
-                        </td>
-                    </tr>
-                    <tr className="border border-gray-300">
-                        
-                        <th className="border border-gray-300 px-4 py-2">Food Name</th>
-                        <td className="border border-gray-300 px-4 py-2">209</td>
-                        <td className="border border-gray-300 px-4 py-2">41</td>
-                        <td className="border border-gray-300 px-4 py-2 text-center">
-                            <button className="text-red-600">🗑️</button>
-                        </td>
-                    </tr>
-                    {/* More rows here */}
                 </tbody>
             </table>
-
         </main >
     );
 }
