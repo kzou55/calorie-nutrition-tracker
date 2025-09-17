ee423ae60f5292b45fdff72390b05a07f89f511b
Ken <kzou55@gmail.com>
2025-09-17 14:00:22 -0400

    implemented initial mock of dashboard page

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -1,15 +1,35 @@
 import type { JSX } from "react";
 import NutritionSummary from "../components/dashboard/NutritionSummary";
-import MealList from "../components/dashboard/MealList";
 import { mockMeal } from "../data/meals";
+import { useSelector } from "react-redux";
+import type { RootState } from "../store/store";
 
 const DashboardPage = (): JSX.Element => {
+  const meals = useSelector((state: RootState) => state.meals);
+
+  const totals = meals.reduce(
+    (acc, meal) => {
+      meal.mealFoodEntries.forEach((entry) => {
+        acc.calories += entry.foodItem.calories;
+        acc.protein += entry.foodItem.protein;
+      });
+      return acc;
+    },
+    {calories: 0, protein: 0}
+  );
+
+
   return (
-    <main className="flex flex-col">
+    <main className="flex-col">
       <h1>Dashboard</h1>
-      <div className="flex justify-evenly">
-        <NutritionSummary meals={mockMeal} />
-        <MealList meals={mockMeal} />
+      <div className="flex justify-evenly border-solid">
+        <h2>
+          Summary for Today
+          <div>
+            <p>Total Calories: {totals.calories}</p>
+            <p>Total Protein: {totals.protein}</p>
+          </div>
+        </h2>
       </div>
     </main>
   );
@@ -17,3 +37,7 @@ const DashboardPage = (): JSX.Element => {
 
 export default DashboardPage;
 
+
+
+// Add total calories for today
+// Add Total Nutritions
